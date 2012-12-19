8792eadd39224a67b3d03d9db8d0f6430d94b127
Kyle Steinfeld <ksteinfe@berkeley.edu>
2012-12-18 23:38:19 -0800

    added everything back

---
+++ vs/Component/PythonEnvironment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Rhino.Runtime;
+
+namespace GhPython.Component
+{
+  public class PythonEnvironment
+  {
+    internal PythonEnvironment(Grasshopper.Kernel.GH_Component component, PythonScript script)
+    {
+      Component = component;
+      Script = script;
+
+      if (script != null)
+      {
+        Type scriptType = script.GetType();
+
+        var scopeField = scriptType.GetField("m_scope");
+        if (scopeField != null)
+        {
+          LocalScope = scopeField.GetValue(script);
+        }
+
+        var baseType = scriptType.BaseType;
+        if (baseType != null && baseType != typeof (object))
+        {
+          var hostType = baseType.Assembly.GetType("RhinoPython.Host");
+          if (hostType != null)
+          {
+            var engineInfo = hostType.GetProperty("Engine");
+            if (engineInfo != null)
+              Engine = engineInfo.GetValue(null, null);
+
+            var scopeInfo = hostType.GetProperty("Scope", System.Reflection.BindingFlags.NonPublic |
+                                                          System.Reflection.BindingFlags.GetProperty |
+                                                          System.Reflection.BindingFlags.Static);
+            if (scopeInfo != null)
+              ScriptScope = scopeInfo.GetValue(null, null);
+          }
+        }
+      }
+
+    }
+
+    public GH_Component Component { get; internal set; }
+
+    public PythonScript Script { get; internal set; }
+
+    public object LocalScope { get; internal set; }
+
+    public object ScriptScope { get; internal set; }
+
+    public object Engine { get; internal set; }
+  }
+}
---
+++ vs/Component/StringList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GhPython.Component
+{
+  /// <summary>
+  /// Used to capture the output stream from an executing python script
+  /// </summary>
+  class StringList
+  {
+    private readonly List<string> _txts = new List<string>();
+
+    public void Write(string s)
+    {
+      _txts.Add(s);
+    }
+
+    public void Reset()
+    {
+      _txts.Clear();
+    }
+
+    public IList<string> Result
+    {
+      get { return new System.Collections.ObjectModel.ReadOnlyCollection<string>(_txts); }
+    }
+
+    public override string ToString()
+    {
+      var sb = new StringBuilder();
+      foreach (string s in _txts)
+        sb.AppendLine(s);
+      return sb.ToString();
+    }
+  }
+}
