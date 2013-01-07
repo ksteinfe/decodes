66d6d987e6987558f0aae6bb227da8bfc8ef22b7
Kyle Steinfeld <ksteinfe+git@gmail.com>
2013-01-07 14:55:22 -0800

    messed around with bat file

--- sandbox/_ks_sandbox/_empty.py
+++ sandbox/_ks_sandbox/_empty.py
@@ -1,32 +1,4 @@
 import decodes.core as dc
 from decodes.core import *
 
-
-def main():
-    print 'hello mains'
-    outie = makeOut(outies.Rhino, "linetest")
-    
-    #print "constructors"
-    p = Point(1,1)
-    v = Vec(0,0,1)
-    seg1 = Segment(p,v)
-    outie.put(seg1)
-    
-    p0 = Point(-1,1)
-    p1 = Vec(-2,1)
-    seg2 = Segment(p0,p1)
-    outie.put(seg2)
-    
-    ray1 = Ray(Point(0,-1),Vec(0,0,1))
-    outie.put(ray1)
-    
-    lin1 = Line(Point(0,1),Vec(0,0,1))
-    outie.put(lin1)
-    
-    
-    
-    
-    outie.draw()
-
-if __name__ == "__main__" : 
-    main()
+vec = Vec()
