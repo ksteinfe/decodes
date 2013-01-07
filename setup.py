20bad5775da5808d851177f2d5881ed1f8855dfa
Kyle Steinfeld <ksteinfe+git@gmail.com>
2013-01-07 12:10:44 -0800

    created batch file for building dists

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-
-
 # written using this tutorial: http://getpython3.com/diveintopython3/packaging.html
+# to build a windows dist, do this: python setup.py bdist_wininst
+# to build a simple dist, do this: python setup.py sdist
 from distutils.core import setup
 setup(
     name = 'decodes',
-    packages = ['decodes','decodes.core', 'decodes.extensions', 'decodes.io'],
+    packages = ['decodes','decodes.core', 'decodes.extensions', 'decodes.io', 'decodes.io.pysvg', 'decodes.io.rhinoscript'],
     version = '0.0.1',
     description = 'a geometry library for 3d designers',
     url = "http://decod.es",
