ab18c4c6438f1999d57d79663a71e6e7edb7e75a
Kyle Steinfeld <ksteinfe+git@gmail.com>
2013-01-08 11:26:47 -0800

    messed around with vs solution

--- vs/DummyForPostBuild/Properties/AssemblyInfo.cs
+++ vs/DummyForPostBuild/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("DummyForPostBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("DummyForPostBuild")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7944f951-8dfc-4ba7-b9de-7d3c682a0eab")]
+[assembly: Guid("238ad707-0516-401d-b618-08d5c924df77")]
 
 // Version information for an assembly consists of the following four values:
 //
