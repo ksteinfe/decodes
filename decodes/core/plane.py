e664002675d2af172809786eaa7603ca80e52bb3
cesandoval <ce.sandoval09@gmail.com>
2013-03-02 20:05:13 -0800

    updated the docstrings with the plane and xform classes

--- decodes/core/plane.py
+++ decodes/core/plane.py
@@ -12,27 +12,74 @@ class Plane(Vec):
     """
     
     def __init__(self, point=Point(), normal=Vec(0,0,1)):
-        """ Constructs a Plane object.
+        """ Plane constructor.
 
+            :param point: Base point for a plane. 
+            :type point: Point
+            :param normal: Normal direction of the new Plane. Defaults to Vec(0,0,1)
+            :type normal: Vec
+            :result: Plane object.
+            :rtype: Plane
         """
         super(Plane,self).__init__(point)
         self._vec = normal.normalized()
 
     
     @property
-    def vec(self): return self._vec
+    def vec(self): 
+        """ Returns the plane's vector.
+
+            :result: Plane's vector (Point).
+            :rtype: Vec
+        """
+        return self._vec
     @vec.setter
-    def vec(self, v): self._vec = v.normalized()
+    def vec(self, v): 
+        """ Sets the plane's vector.
+
+            :param v: Sets the vector of the plane (Point).
+            :type v: Vec, point
+            :result: Plane object.
+            :rtype: Plane
+        """
+        self._vec = v.normalized()
 
     @property
-    def normal(self): return self._vec
+    def normal(self): 
+        """ Returns the plane's normal.
+
+            :result: Plane's normal.
+            :rtype: Vec
+        """
+        return self._vec
     @normal.setter
-    def normal(self, v): self._vec = v.normalized()
+    def normal(self, v): 
+        """ Sets the plane's normal.
+
+            :param v: Sets the normal of the plane .
+            :type v: Vec
+            :result: Plane object.
+            :rtype: Plane
+        """
+        self._vec = v.normalized()
 
     @property
-    def cpt(self): return Point(self.x,self.y,self.z)
+    def cpt(self): 
+        """ Returns the plane's center point.
+
+            :result: Plane's center point.
+            :rtype: Point
+        """
+        return Point(self.x,self.y,self.z)
     @cpt.setter
     def cpt(self, pt): 
+        """ Sets the plane's center point.
+
+            :param pt: Sets the center point of the plane .
+            :type pt: Point
+            :result: Plane object.
+            :rtype: Plane
+        """
         self.x = pt.x
         self.y = pt.y
         self.z = pt.z
