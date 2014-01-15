49180788f3e2b1cb297c047ddc0404569acf0752
Luis Jaggy <luis.jaggy@gmail.com>
2014-01-15 10:40:40 -0800

    graph class

---
+++ decodes/extensions/graph.py
@@ -0,0 +1,24 @@
+import decodes.core as dc
+from decodes.core import *
+import math
+
+class Graph(object):
+    """
+    A simple undirected, weighted graph
+    """
+    def __init__(self):
+        self.nodes = set()
+        self.edges = {}
+        self.distances = {}
+    
+    def add_node(self, value):
+        self.nodes.add(value)
+    
+    def add_edge(self, from_node, to_node, distance):
+        self._add_edge(from_node, to_node, distance)
+        self._add_edge(to_node, from_node, distance)
+ 
+    def _add_edge(self, from_node, to_node, distance):
+        self.edges.setdefault(from_node, [])
+        self.edges[from_node].append(to_node)
+        self.distances[(from_node, to_node)] = distance
