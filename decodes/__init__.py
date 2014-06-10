36bf37a6636ba2b6934d85701b6cf3c092c4098d
Kyle Steinfeld <ksteinfe@gmail.com>
2014-06-10 13:07:39 -0700

    added 3js modificaitons

--- decodes/__init__.py
+++ decodes/__init__.py
@@ -8,7 +8,7 @@ print "http://decod.es"
 
 class Outies:
     # list here all the outies we currently support
-    Rhino, Grasshopper, SVG , ACAD, Dynamo = range(5)
+    Rhino, Grasshopper, SVG , ACAD, Dynamo,ThreeJS = range(6)
 
 
 # keep this up to date with what outies we support
@@ -48,6 +48,12 @@ def make_out(outtype, name="untitled", path=False, **kargs):
         
         if path : return io.svg_out.SVGOut(name, path, canvas_dimensions=c_dim, flip_y=flip,save_file=save_file)
         else : return io.svg_out.SVGOut(name, canvas_dimensions=c_dim, flip_y=flip,save_file=save_file)
+    elif outtype == Outies.ThreeJS:
+        import io.threejs_out
+        save_file=True
+        if "save_file" in kargs : save_file = kargs["save_file"]
+        if path : return io.threejs_out.ThreeJSOut(name, path, save_file=save_file)
+        else : return io.threejs_out.ThreeJSOut(name, save_file=save_file)
     elif outtype == Outies.Dynamo:
         import io.dynamo_out
         return io.dynamo_out.DynamoOut()
---
+++ sandbox/_kd sandbox/threejs_test.py
@@ -0,0 +1,6 @@
+import decodes
+from decodes.core import *
+
+out = decodes.make_out(decodes.Outies.ThreeJS,"myscene","C:\\")
+out.put(Point(2,2,4))
+out.draw()
