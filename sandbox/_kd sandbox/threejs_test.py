bd6d13dffc301de2a8f236f06d804234ed225db7
kartikeyadate <kartikeya.date@gmail.com>
2014-06-16 11:59:26 -0700

    ?

--- sandbox/_kd sandbox/threejs_test.py
+++ sandbox/_kd sandbox/threejs_test.py
@@ -4,4 +4,10 @@ from decodes.core import *
 out = decodes.make_out(decodes.Outies.ThreeJS,"myscene","C:\\")
 out.put(Point(2,2,4))
 out.draw()
-pass
+
+# {
+# "metadata":{"version":4.3, "type":"Object","generator":"ObjectExporter"},
+# "geometries": [{ "uuid": "Some number", "x":  , "y":  , "z":  }],
+# "materials":[{}],
+# "object":{ , , ,"children":[{},{}]}
+# }
