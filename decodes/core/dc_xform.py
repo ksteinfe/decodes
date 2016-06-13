66a27656c7f6c013c9c65e061a8eb72a7a53dd31
Joy Ko <jko01@risd.edu>
2016-06-13 15:02:25 -0400

    reinstated xforms for haspts

--- decodes/core/dc_xform.py
+++ decodes/core/dc_xform.py
@@ -255,7 +255,7 @@ class Xform(object):
         # HASPTS GEOMETRY
         # applies transformation to the verts, leaving the basis intact
         if isinstance(other, HasPts) : 
-            raise NotImplementedError("can't xform a haspts")
+            #raise NotImplementedError("can't xform a haspts")
             other._verts = [v*self for v in other._verts]
             return other
             # TODO: deal with applying transformations to haspts geometry
